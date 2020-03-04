7a73b54f3084c9f971295b0da07dbdf784c81982
golbpw <61608274+golbpw@users.noreply.github.com>
2020-03-04 09:11:45 +0900

    Create testjs.user.js

---
+++ testjs.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         TestJs
+// @namespace    http://golbpw.wpblog.jp
+// @description  webhook test
+// @author       golbpw
+// @licence      MIT
+// @include      *
+// @version      1.0.0
+// ==/UserScript==
+
+
+(()=>{
+	console.log('test');
+})();
