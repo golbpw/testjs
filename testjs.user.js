410dad1ce16665159145699ed70b646302a4af0c
golbpw <61608274+golbpw@users.noreply.github.com>
2020-03-17 00:34:54 +0900

    Update testjs.user.js

--- testjs.user.js
+++ testjs.user.js
@@ -10,7 +10,7 @@
 // @grant        GM.getResourceUrl
 // @resource     data https://raw.githubusercontent.com/golbpw/testjs/master/data.json?latest
 // @include      *
-// @version      1.0.15
+// @version      1.0.16
 // ==/UserScript==
 
 
