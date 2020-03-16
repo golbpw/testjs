42176b32bdd7e70c09af480531100714d729f8ec
golbpw <61608274+golbpw@users.noreply.github.com>
2020-03-16 23:48:43 +0900

    Update testjs.user.js

--- testjs.user.js
+++ testjs.user.js
@@ -4,11 +4,13 @@
 // @description  webhook test
 // @author       golbpw
 // @license      MIT
+// @homepageURL  https://github.com/golbpw
+// @supportURL   https://gist.github.com/golbpw
 // @grant        GM_getResourceText
 // @grant        GM.getResourceUrl
 // @resource     data https://github.com/golbpw/testjs/raw/master/data.json#1.0.5
 // @include      *
-// @version      1.0.5
+// @version      1.0.6
 // ==/UserScript==
 
 
