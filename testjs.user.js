abb594213a8ad07b03a48e4fffc1442aeb445114
golbpw <61608274+golbpw@users.noreply.github.com>
2020-03-16 23:31:47 +0900

    Update testjs.user.js

--- testjs.user.js
+++ testjs.user.js
@@ -4,11 +4,20 @@
 // @description  webhook test
 // @author       golbpw
 // @license      MIT
+// @grant        GM_getResourceText
+// @grant        GM.getResourceUrl
+// @resource     data https://github.com/golbpw/testjs/raw/master/data.json
 // @include      *
-// @version      1.0.1
+// @version      1.0.2
 // ==/UserScript==
 
 
-(()=>{
-	console.log('github port test');
+(async () => {
+	console.log('TestJs: github port test');
+
+	const myData = JSON.parse(GM_getResourceText(data));
+	console.info('getResourceText', myData);
+
+	const myDataUrl = await GM.getResourceUrl(data);
+	console.info('getResourceUrl', myDataUrl);
 })();
